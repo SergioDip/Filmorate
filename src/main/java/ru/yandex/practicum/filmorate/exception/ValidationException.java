e0d25858f7fb094c80014f855d176fff508b43e7
SergioDip <102463586+SergioDip@users.noreply.github.com>
2022-09-29 20:51:12 +0300

    Revert "Sprint#9"

--- src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
+++
@@ -1,8 +0,0 @@
-package ru.yandex.practicum.filmorate.exception;
-
-public class ValidationException extends Exception{
-
-	public ValidationException(final String message) {
-		super(message);
-	}
-}
